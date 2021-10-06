356609b1dc6d2e198e7cd9d331eb8551dcdbda10
malinoile <filippov.malinoil@yandex.ru>
2021-10-07 00:59:43 +1000

    Initial commit

---
+++ app/src/main/java/ru/malinoile/motiontest/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.malinoile.motiontest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
